dbddeb5c2e8d51d9caa9b29452964ed59a0dc076
Adam R <adam@rosenblatt.ca>
2025-10-11 01:06:33 -0400

    Ability to simulate message being sent to pico from another pico

--- chat.py
+++ chat.py
@@ -1,3 +1,5 @@
+# Uses serial blocking input given in announcement
+
 from machine import Pin, UART
 import time
 import uasyncio as asyncio
@@ -7,17 +9,21 @@ uart = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9))
 uart.init(bits=8, parity=None, stop=1) 
 
 # Configure button input with internal pull-up
-button = Pin(13, Pin.IN, Pin.PULL_DOWN)
+blocking_button = Pin(13, Pin.IN, Pin.PULL_DOWN)
+# Will simulate message being sent from other pico for self testing
+sim_button = Pin(11, Pin.IN, Pin.PULL_DOWN)
 
 data_to_send = "Button pressed!"
 print("Press the button to send message over UART")
-prev_state = 0
+blocking_prev_state = 0
+sim_prev_state = 0
 
 while True:
-    current_state = button.value()
+    blocking_current_state = blocking_button.value()
+    sim_current_state = sim_button.value()
 
     # Detect button press (HIGH to LOW)
-    if (prev_state == 1 and current_state == 0):
+    if (blocking_prev_state == 1 and blocking_current_state == 0):
         # get (blocking) user input
         data_to_send = input("Data to send? ")
         uart.write(data_to_send.encode('utf-8'))
@@ -26,7 +32,15 @@ while True:
         # Debounce delat
         time.sleep(0.3)
     
-    prev_state = current_state
+    # Detect button press (HIGH to LOW)
+    if (sim_prev_state == 1 and sim_current_state == 0):
+        # send message to pico
+        uart.write("Message from other pico")
+
+        time.sleep(0.3)
+
+    blocking_prev_state = blocking_current_state
+    sim_prev_state = sim_current_state
     time.sleep(0.01)
 
     # Check for any incoming UART data
